2bc90f8c8193da2b9f5da01d8831f7964c8d2dd6
Katya Shilovskaya <kat6123@yandex.ru>
2017-02-24 21:39:15 +0300

    add print

--- lab.py
+++ lab.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-import sys
-
 
 def main():
-    print "Hello, Katya!"
-    print "This is {name}".format(name=sys.argv[0])
+    for i in range(7):
+        print '*'*i
 
 if __name__ == "__main__":
     main()
