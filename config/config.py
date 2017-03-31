27c8f983046f56af2d42485d441b31c04b8bf9b6
Katya Shilovskaya <kat6123@yandex.ru>
2017-03-31 09:38:28 +0300

    add init by user config file

--- config/config.py
+++ config/config.py
@@ -1,7 +1,32 @@
+# -*- coding: utf-8 -*-
+from ConfigParser import ConfigParser
+from argparse import ArgumentParser
+from sys import argv
+
+
 class Config(object):
     """Contains launch configuration"""
-    def __init__(self, arg):
-        self.arg = arg
+    user_config_path = "config/smart_rm.cfg"
+    json_config_path = "config/smart_rm.json"
 
-    def _get_command_line_config():
+    def __init__(self):
+        self.location = {}
+        self.modes = {}
+        self.politics = {}
+        self.recycle_basket_options = {}
+
+        self._get_user_config()
+        self._get_command_line_config()
+        self._get_json_config()
+
+    def _get_command_line_config(self):
+        pass
+
+    def _get_user_config(self, path=user_config_path):
+        config = ConfigParser()
+        config.read(path)
+        self.location = dict(config.items("location"))
+        self.modes = dict(config.items("modes"))
+
+    def _get_json_config(self):                             # TODO
         pass
