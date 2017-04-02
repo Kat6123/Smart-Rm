fef63b8e1b74aeb92edfefba90e480c4d36e6fe3
Katya Shilovskaya <kat6123@yandex.ru>
2017-04-02 11:51:22 +0300

    add permanent deleting for basket cleaning

--- remove.py
+++ remove.py
@@ -2,28 +2,52 @@
 from error import Error
 from os import (
     remove,
-    rmdir
+    rmdir,
+    listdir,
+    unlink
 )
-
+from os.path import(
+    join,
+    islink,
+    isfile)
 from shutil import rmtree
 
 
-def rm_file(path):
+def remove_file(path):
     try:
         remove(path)
     except OSError as e:
         raise Error(e)
 
 
-def rm_dir(path):
+def remove_dir(path):
     try:
         rmdir(path)
     except OSError as e:
         raise Error(e)
 
 
-def rm_tree(path):
+def remove_tree(path):
     try:
         rmtree(path)
     except OSError as e:
         raise Error(e)
+
+
+def remove_link(path):
+    try:
+        unlink(path)
+    except OSError as e:
+        raise Error(e)
+
+
+def remove_directory_content(directory):
+    content = listdir(directory)
+    for path in content:
+        abs_path = join(directory, path)
+        if islink(abs_path):
+            remove_link(abs_path)
+        elif isfile(abs_path):
+            remove_file(abs_path)
+        else:
+            remove_tree(abs_path)
