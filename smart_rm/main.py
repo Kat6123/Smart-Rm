d2304244251c015d5c0493f3e686164abc6b13e9
Katya Shilovskaya <kat6123@yandex.ru>
2017-04-22 22:43:15 +0300

    Modify main to call remove functionality

--- smart_rm/main.py
+++ smart_rm/main.py
@@ -1,25 +1,36 @@
-#!/usr/bin/env python2.7
+#! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 from log.logger import tune_logger
-from logging import (
-    debug,
-    info,
-    warning,
-    error,
-    critical,
-    DEBUG,
-    INFO
-)
+from config.set import Config
+from smart_rm import AdvancedRemover
 
 
 def main():
-    tune_logger(log_level=DEBUG, logfile_path="file")
+    config = Config()
 
-    debug("debug")
-    info("info")
-    warning("warning")
-    error("error")
-    critical("critical")
+    if config.modes["silent"]:          # XXX
+        tune_logger(write_to_stderr=False,
+                    logfile_path=config.file_paths_to["log"])
+        config.modes["not_confirm_rm"] = True
+    else:
+        tune_logger(logfile_path=config.file_paths_to["log"])
+
+    remover = AdvancedRemover(
+        config.file_paths_to["basket"],
+        config.modes["confirm_rm_always"],
+        config.modes["not_confirm_rm"],
+        config.modes["confirm_rm_if_file_write_ban"],
+        config.modes["dry_run"],
+        config.modes["follow_sym_link"]
+    )
+
+    if config.actions["remove"]["tree"]:
+        remover.remove_trees(config.file_paths_to["remove"])
+    elif config.actions["remove"]["directory"]:
+        remover.remove_directories(config.file_paths_to["remove"])
+    elif config.actions["remove"]["file"]:
+        remover.remove_files(config.file_paths_to["remove"])
+
 
 if __name__ == '__main__':
     main()
