1c7fef7ceda0f5deb3662a04d3a4ba257f356ed2
Katya Shilovskaya <kat6123@yandex.ru>
2017-04-25 21:35:14 +0300

    Add OtherOSError for non-specialized operation system errors

--- smart_rm/error.py
+++ smart_rm/error.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-from os import linesep
 
 
-class Error (Exception):         # TODO:
-    def __init__(self, errors):
-        self.errors = errors
+class SmartRMError(EnvironmentError):
+    pass
 
-    def __str__(self):
-        result = linesep.join(self.errors)
-        return result
+
+class PermissionError(SmartRMError):
+    pass
+
+
+class ExistError(SmartRMError):
+    pass
+
+
+class ModeError(SmartRMError):
+    pass
+
+
+class OtherOSError(SmartRMError):
+    pass
