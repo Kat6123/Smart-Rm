36b65f5f1948837a56c163739281aea64d4dc4cf
Katya Shilovskaya <kat6123@yandex.ru>
2017-05-05 19:15:59 +0300

    Put help functions in separate file

---
+++ smart_rm/utils.py
@@ -0,0 +1,27 @@
+import errno
+import os
+import os.path
+import re
+from error import (
+    PermissionError,
+    ExistError
+)
+
+
+def check_path_existance(path):
+    abs_path = os.path.abspath(path)
+
+    if not os.access(os.path.dirname(abs_path), os.R_OK):
+        raise PermissionError(
+            errno.EACCES, os.strerror(errno.EACCES), os.path.dirname(abs_path)
+        )
+    elif not os.path.exists(abs_path):
+        raise ExistError(errno.ENOENT, os.strerror(errno.ENOENT), abs_path)
+
+
+def get_regex_matcher(regex=".*a$"):
+    def match_path(path):
+        matches = re.match(regex, os.path.basename(path))
+        if matches:
+            return matches.group(0)
+    return match_path
