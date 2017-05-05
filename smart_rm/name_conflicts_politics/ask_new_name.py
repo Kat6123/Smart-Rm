3ae4f8524aeee32bd176361f6b135802a16b02af
Katya Shilovskaya <kat6123@yandex.ru>
2017-05-05 19:16:38 +0300

    Create separate folder for politics

---
+++ smart_rm/name_conflicts_politics/ask_new_name.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+class MoveAndAskNewNameForMovableObject(Mover):
+    def _watch(self, path, destination):
+        super(MoveAndRaiseExceptionIfSamenameFiles, self)._watch(
+            path,
+            destination
+        )
+        if self.exists:
+            new_name = raw_input("Enter new name for {0}:".format(path))
+            if new_name:
+                self.final_path = join(self.destination, new_name)
+            else:
+                raise OtherOSError(             # Or just message?
+                    "Already exists {0} in {1}"
+                    "".format(self.source, self.destination)
+                )
+
+    def _do(self):
+        move_tree(self.source, self.final_path)
+        return self.final_path
