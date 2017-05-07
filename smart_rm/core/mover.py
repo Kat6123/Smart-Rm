326fd10c3ffec1dc3dc581ae753a4d289ed4b3d8
Katya Shilovskaya <kat6123@yandex.ru>
2017-05-08 00:33:32 +0300

    Refactor mover.py

--- smart_rm/core/mover.py
+++ smart_rm/core/mover.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 import errno
+import logging
 import os
 import os.path
 import stat
+
 from smart_rm.core.error import (
     AccessError,
+    ExistError,
     SystemError
 )
-from smart_rm.core.remove import (
-    move_tree
-)
-from smart_rm.utils.check import (
-    check_path_existance
-)
-from smart_rm.utils.constants import (
-    ROOT
-)
+from smart_rm.utils.simple_os_actions import move_tree
+from smart_rm.utils.constants import ROOT
 
 
 class DryRunMixin(object):
@@ -23,29 +19,28 @@ class DryRunMixin(object):
         self,
         dry_run=False
     ):
-        if dry_run:         # ? move to
-            self.move = self.dry_run
+        if dry_run:
+            self.run = self._dry_run
         else:
-            self.move = self.execute
+            self.run = self._execute
 
-    def dru_run(self, *args, **kwargs):
-        self._watch(*args, **kwargs)
+    def _dry_run(self, *args, **kwargs):
+        self.watch(*args, **kwargs)
 
-    def execute(self, *args, **kwargs):
-        self._watch(*args, **kwargs)
-        return self._do()
+    def _execute(self, *args, **kwargs):
+        self.watch(*args, **kwargs)
+        return self.do()
 
 
-class Mover(DryRunMixin):           # shutil.move to directory
+class Mover(DryRunMixin):
     special_file_modes = [
         stat.S_ISBLK, stat.S_ISCHR,
         stat.S_ISFIFO, stat.S_ISSOCK
     ]
 
-    root_inners = [
-        os.path.abspath(root_inner) for root_inner in os.listdir(ROOT)
-    ]
-    special_directories = root_inner.append(ROOT)
+    special_directories = [
+        os.path.join(ROOT, root_inner) for root_inner in os.listdir(ROOT)
+    ] + [ROOT]
 
     def __init__(self, dry_run=False):
         super(Mover, self).__init__(dry_run)
@@ -53,11 +48,15 @@ class Mover(DryRunMixin):           # shutil.move to directory
         self.source = self.destination = self.final_path = ""
         self.already_exists = False
 
-    def _watch(self, source, destination):
+        self.move = self.run
+
+    def watch(self, source, destination):
+        logging.info("Start checking")
+
         self.tune_paths(source, destination)
 
-        check_path_existance(self.source)
-        check_path_existance(self.destination)
+        self.check_path_existance(self.source)
+        self.check_path_existance(self.destination)
 
         self.check_distance_is_directory()
         self.check_directory_access(os.path.dirname(self.source))  # Improve
@@ -72,12 +71,15 @@ class Mover(DryRunMixin):           # shutil.move to directory
         self.check_empty_space()
 
         if os.path.exists(self.final_path):
+            logging.warning("Already exists \"{0}\"".format(self.final_path))
             self.already_exists = True
 
-    def _do(self):
+        logging.info("Stop checking")
+
+    def do(self):
         if self.already_exists:
             raise SystemError(
-                "Already exists {0} in {1}"
+                "Already exists \"{0}\" in \"{1}\""
                 "".format(self.source, self.destination)
             )
         move_tree(self.source, self.final_path)
@@ -94,20 +96,49 @@ class Mover(DryRunMixin):           # shutil.move to directory
             )
         )
 
+    def check_path_existance(self, path):
+        logging.info("Check \"{0}\" existance".format(path))
+
+        if not os.access(os.path.dirname(path), os.R_OK):
+            raise AccessError(
+                errno.EACCES, os.strerror(errno.EACCES), os.path.dirname(path)
+            )
+        elif not os.path.exists(path):
+            raise ExistError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+
+        logging.info("Pass check")
+
     def check_distance_is_directory(self):
+        logging.info(
+            "Check distance \"{0}\" is directory"
+            "".format(self.destination)
+        )
+
         if not os.path.isdir(self.destination):
             raise SystemError(
-                "Distance {0} is not directory"
+                "Distance \"{0}\" is not directory"
                 "".format(os.path.basename(self.destination))
             )
 
+        logging.info("Pass check")
+
     def check_directory_access(self, directory):
-        if not os.access(directory, os.W_OK) and os.access(directory, os.X_OK):
+        logging.info(
+            "Check directory \"{0}\" has \"write\" and \"execute\" access"
+            "".format(directory)
+        )
+
+        if not (os.access(directory, os.W_OK) and
+                os.access(directory, os.X_OK)):
             raise AccessError(
-                errno.EACCES, os.strerror(errno.EACCES), file
+                errno.EACCES, os.strerror(errno.EACCES), directory
             )
 
+        logging.info("Pass check")
+
     def check_special_file(self):
+        logging.info("Check file \"{0}\" is general".format(self.source))
+
         if os.getuid:                      # Explain!
             mode = os.stat(self.source).st_mode
             for special_mode in self.special_file_modes:
@@ -116,7 +147,13 @@ class Mover(DryRunMixin):           # shutil.move to directory
                         errno.EACCES, "Not regular file", self.source
                     )
 
-    def check_system_directory(self):
+        logging.info("Pass check")
+
+    def check_system_directory(self):          # remove
+        logging.info(
+            "Check system directory \"{0}\"".format(self.source)
+        )
+
         if os.getuid:
             for special_directory in self.special_directories:
                 if self.source == special_directory:
@@ -126,13 +163,19 @@ class Mover(DryRunMixin):           # shutil.move to directory
             if os.path.ismount(self.source):
                 raise AccessError(errno.EACCES, "Mount point", self.source)
 
+        logging.info("Pass check")
+
     def check_cycle(self):          # Explain in doc!
+        logging.info("Check name cycle \"{0}\"".format(self.source))
+
         prefix = os.path.commonprefix([self.source, self.destination])
         if prefix == self.source:
             raise SystemError(
-                "Cannot move {0} into itself {1}"
+                "Cannot move \"{0}\" into itself \"{1}\""
                 "".format(self.source, self.destination)
             )
+
+        logging.info("Pass check")
 
     def check_empty_space(self):
         pass
