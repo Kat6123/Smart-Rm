879abfbb271db537b294d2c9afd10428a580462f
Katya Shilovskaya <kat6123@yandex.ru>
2017-05-30 17:19:29 +0300

    Add mains

--- simple_rm/main_remove.py
+++ simple_rm/main_remove.py
@@ -1,23 +1,59 @@
 #! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+
+import simple_rm.constants as const
+
 from simple_rm.config import (
     Config,
-    ArgumentParser
+    get_config_dict_from_remove_parser_namespace,
+    get_config_from_file,
+    set_parser_for_remove,
+    set_parser_with_common_flags
 )
-from trash import Trash
+from simple_rm.check import (
+    get_correct_path,
+    return_true
+)
+from simple_rm.wrapper import (
+    init_trash_by_config,
+    report_about_result
+)
+from simple_rm.logger import set_logger
 
 
 def main():
     config = Config()
+
     parser = ArgumentParser()
-    config.override(parser.get_config())
+    set_parser_for_remove(parser)
+    set_parser_with_common_flags(parser)
+    args = parser.parse_args()
 
-    trash = Trash(
-        trash_location=config.path_to["trash"],
-        remove_mode=config.remove_mode
-    )
+    config.update(get_config_dict_from_remove_parser_namespace(args))
 
-    trash.remove(parser.args.path)
+    if args.config_file_path:
+        config.update(
+            get_config_from_file(get_correct_path(args.config_file_path))
+        )
+
+    if args.regex:
+        config.remove["aim"] = const.REMOVE_TREE_MODE
+
+    trash = init_trash_by_config(config)
+
+    log_file = args.log_file_path
+    if args.imitation:
+        args.verbose = True
+    if args.silent_mode:
+        trash.confirm_removal = return_true
+        set_logger(write_to_stderr=False)
+    elif args.verbose:
+        set_logger(log_level="info", logfile_path=log_file)
+    else:
+        set_logger(logfile_path=log_file)
+
+    report_about_result(trash.remove(args.path, regex=args.regex))
 
 
 if __name__ == '__main__':
--- simple_rm/main_trash.py
+++ simple_rm/main_trash.py
@@ -1,24 +1,59 @@
 #! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
 from simple_rm.config import (
     Config,
-    ArgumentParser
+    get_config_dict_from_trash_parser_namespace,
+    get_config_from_file,
+    set_parser_for_trash,
+    set_parser_with_common_flags
 )
-from trash import Trash
+from simple_rm.check import (
+    get_correct_path,
+    return_true
+)
+from simple_rm.wrapper import (
+    init_trash_by_config,
+    report_about_result
+)
+from simple_rm.logger import set_logger
 
 
 def main():
     config = Config()
+
     parser = ArgumentParser()
-    config.override(parser.get_config())
+    set_parser_for_trash(parser)
+    set_parser_with_common_flags(parser)
+    args = parser.parse_args()
 
-    trash = Trash(
-        trash_location=config.path_to["trash"],
-        remove_mode=config.remove_mode
-    )
+    config.update(get_config_dict_from_trash_parser_namespace(args))
 
-    if parser.args.
-    trash.remove(parser.args.path)
+    if args.config_file_path:
+        config.update(
+            get_config_from_file(get_correct_path(args.config_file_path))
+        )
+
+    trash = init_trash_by_config(config)
+
+    log_file = args.log_file_path
+
+    if args.imitation or args.view_trash_content:
+        args.verbose = True
+    if args.silent_mode:
+        trash.confirm_removal = return_true
+        set_logger(write_to_stderr=False)
+    elif args.verbose or not (args.restore_from_trash or args.clear_trash):
+        set_logger(log_level="info", logfile_path=log_file)
+    else:
+        set_logger(logfile_path=log_file)
+
+    if args.restore_from_trash:
+        report_about_result(trash.restore(args.restore_from_trash))
+    elif args.clear_trash:
+        report_about_result(trash.clean())
+    else:
+        report_about_result(trash.view())
 
 
 if __name__ == '__main__':
