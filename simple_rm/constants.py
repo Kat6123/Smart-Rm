e5ba78e49672241e43ec1391efae5bc908a8705c
Katya Shilovskaya <kat6123@yandex.ru>
2017-05-30 17:25:49 +0300

    Modify constants

--- simple_rm/constants.py
+++ simple_rm/constants.py
@@ -18,7 +18,10 @@ TRASH_INFO_DIRECTORY = "info"
 INFO_SECTION = "Trash info"
 FILE_HASH_OPTION = "Hash"
 OLD_PATH_OPTION = "Path"
+SIZE_OPTION = "Size"
 REMOVE_DATE_OPTION = "Date"
+DATE_FORMAT = "%d.%m.%Y  %H:%M"
+TIME_DELTA_FORMAT = "%W months %d days %H hours %M minutes"
 
 INFO_FILE_EXPANSION = ".trashinfo"
 
@@ -28,8 +31,12 @@ LOG_FILE_LOCATION = "/var/log/smart_rm.log"
 PATH_TO_COPY_DEFAULT_CONFIG = "config/configuration"
 TRASH_LOCATION = "~/.local/share/trash"
 
-SM_REMOVE_SOLVE_NAME_CONFLICT = "MoveAndMakeNewNameDependingOnAmount"
-TRASH_CLEAN_POLITIC = ""
-TRASH_SOLVE_NAME_CONFLICT = "MoveAndAskNewNameForMovableObject"
-
 END_OF_STRING = "$"
+EMPTY_STRING = ""
+
+DEFAULT_CLEAN_POLICY = "remove_all"
+MAX_TRASH_SIZE_IN_BYTES = 2 ** 30
+MAX_TIME_IN_TRASH = "12 months 1 days 1 hours 1 minutes"
+CLEAN_POLICY_TEMPLATE = "get_object_list_by_{0}_policy"
+
+SOLVE_NAME_CONFLICT_POLICY = "give_new_name_depending_on_same_amount"
