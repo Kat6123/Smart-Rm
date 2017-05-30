eae9242da88478a7758a7f18fb7fa90103902880
Katya Shilovskaya <kat6123@yandex.ru>
2017-05-30 17:24:00 +0300

    Modify setup for two entry-points

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setuptools.setup(
     name='simple_rm',
     packages=setuptools.find_packages(),
     long_description=open(
-        os.path.join(os.path.dirname(__file__), 'README.rst')
+        os.path.join(os.path.dirname(__file__), 'README.md')
     ).read(),
     include_package_data=True,
     test_suite="test",
     entry_points={
         'console_scripts': [
-            'smart_rm = simple_rm.main_remove:main'
-            # 'trash = smart_rm.trash_main:main'
+            'smart_rm = simple_rm.main_remove:main',
+            'trash = simple_rm.main_trash:main'
             ]
     }
 )
