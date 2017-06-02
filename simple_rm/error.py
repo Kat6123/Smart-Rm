89597cfda8c4bf556149a915dacc3141d6a41368
Katya Shilovskaya <kat6123@yandex.ru>
2017-06-02 21:38:05 +0300

    Add documentation for errors

--- simple_rm/error.py
+++ simple_rm/error.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
+"""Contain library errors."""
 
 
 class SmartError(EnvironmentError):
+    """SmartError is super class for other module errors.
+
+    It inherits from built-in EnvironmentError. That is why contains
+    same attributes. And can be instantiated with:
+        error code, message string, source file.
+    For more information look 'EnvironmentError' documentation.
+    """
     pass
 
 
 class AccessError(SmartError):
+    """Raises when file or directory has no permissions."""
     pass
 
 
 class ExistError(SmartError):
+    """Raises when file or directory doesn't exist"""
     pass
 
 
 class ModeError(SmartError):
+    """Raises when wrong paths in method remove in Trash class."""
     pass
 
 
 class SysError(SmartError):
+    """Raises when other errors which are connected with operation system."""
     pass
